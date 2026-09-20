a6c0d1a9fcc4ec0a1d68de3979932a933993bf0c
Julian Acuña <142070366+JulianAcun@users.noreply.github.com>
2026-09-19 20:06:40 -0500

    Day 2: Data capture using Postman

--- webhook.php
+++ webhook.php
@@ -1,3 +1,15 @@
 <?php
-echo "Servidor de tickets funcionando";
+// 1. Capturamos la informacion cruda que nos estan enviando
+$datos_crudos = file_get_contents('php://input');
+
+//2. Verificamos si realmente enviaron algo
+if($datos_crudos){
+    //3. Guardamos esos datos en un archivo de texto para poder leerlos (esto es clave para QA y debugging)
+    file_put_contents('log_mensajes.txt',$datos_crudos .PHP_EOL, FILE_APPEND);
+    //4. Respondemos al sistema que envio el mensaje(Postman o Whatsapp)
+    echo "Exito: Los datos fueron recibidos y guardados.";
+
+}else {
+    echo "El servidor funciona, pero no enviaste ningun dato.";
+}
 ?>
